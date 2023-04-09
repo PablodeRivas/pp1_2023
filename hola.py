5aa4a079968d350b30773698e048f1af6b06cdc2
PablodeRivas <p.derivas@itecriocuarto.org.ar>
2023-04-09 17:25:47 -0300

    arregla strings de loop

--- hola.py
+++ hola.py
@@ -1,5 +1,5 @@
 print('hola mundo 2')
 print('parte 2 del hola mundo')
 for i in range(10):
-	print(f'este es el loop numero {i+1})
-print('fin del loop)
+	print(f'este es el loop numero {i+1}')
+print('fin del loop')
